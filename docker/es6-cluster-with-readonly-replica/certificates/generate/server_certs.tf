796c9059116103e1fa483a9ee39a3c00d195e7de
Kristofer Nissbrandt <kristofer.nissbrandt@collectorbank.com>
2021-02-12 10:26:40 +0100

    Added subscription demo

---
+++ docker/es6-cluster-with-readonly-replica/certificates/generate/server_certs.tf
@@ -0,0 +1,44 @@
+locals {
+  dns_names    = ["localhost"]
+  ip_addresses = ["127.0.0.1", "172.16.0.11", "172.16.0.12", "172.16.0.13", "172.16.0.14", "172.16.0.15"]
+}
+
+resource "tls_private_key" "server" {
+  algorithm = "RSA"
+  rsa_bits = "2048"
+}
+
+resource "tls_cert_request" "server" {
+  key_algorithm = tls_private_key.server.algorithm
+  private_key_pem = tls_private_key.server.private_key_pem
+
+  subject {
+    common_name = "eventstoredb-node"
+  }
+
+  dns_names = local.dns_names
+  ip_addresses = local.ip_addresses
+}
+
+resource "tls_locally_signed_cert" "server" {
+  cert_request_pem = tls_cert_request.server.cert_request_pem
+
+  ca_key_algorithm = tls_private_key.root.algorithm
+  ca_private_key_pem = tls_private_key.root.private_key_pem
+  ca_cert_pem = tls_self_signed_cert.root.cert_pem
+
+  validity_period_hours = 43800
+  early_renewal_hours = 8760
+
+  allowed_uses = ["server_auth"]
+}
+
+resource "local_file" "server_cert" {
+  filename = "../server/server.pem"
+  content = tls_locally_signed_cert.server.cert_pem
+}
+
+resource "local_file" "server_key" {
+  filename = "../server/server.key"
+  content = tls_private_key.server.private_key_pem
+}
---
+++ src/esdb6/ESdb6.Subscriptions.Console/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EventStore.Client;
+using EventStore.Examples.Infrastructure.ESdb6;
+using EventStore.Examples.Infrastructure.Logging;
+using Serilog;
+
+namespace ESdb6.Subscriptions.Console
+{
+    internal static class Program
+    {
+        private static EventStoreClient? _client;
+        private static long _numberOfEvents = 0;
+        private static Position? _endPosition;
+        
+        public static async Task Main(string[] args)
+        {
+            SerilogConsoleConfiguration.Configure();
+
+            _client = EventStoreConnectionFactory.Create("esdb://127.0.0.1:2113");
+
+            var endEvents = await _client.ReadAllAsync(Direction.Backwards, Position.End, 1).ToListAsync();
+            _endPosition = endEvents.FirstOrDefault().OriginalPosition;
+
+            await foreach (var resolvedEvent in _client.ReadAllAsync(Direction.Forwards, Position.Start, configureOperationOptions: options => options.TimeoutAfter = TimeSpan.MaxValue))
+            {
+                await ProcessEvent(resolvedEvent);
+                
+                if (resolvedEvent.OriginalPosition == _endPosition)
+                {
+                    Log.Information("LIVE, number of events {NumberOfEvents}", _numberOfEvents);
+                    break;
+                }
+            }
+            
+            Log.Information("Starting subscription from {Position}", _endPosition);
+            var subscription = await _client.SubscribeToAllAsync(
+                _endPosition ?? Position.Start, 
+                EventAppeared
+                ,filterOptions: new SubscriptionFilterOptions(StreamFilter.Prefix("company-", "credit-"))
+                );
+
+            System.Console.ReadLine();
+            
+            Log.Information("Total number of events processed: {NumberOfEvents}", _numberOfEvents);
+            subscription.Dispose();
+        }
+
+        private static async Task EventAppeared(StreamSubscription subscription, ResolvedEvent resolvedEvent, CancellationToken cancellationToken)
+        {
+            if (resolvedEvent.OriginalPosition == _endPosition)
+                return;
+            await ProcessEvent(resolvedEvent);
+        }
+
+        private static Task ProcessEvent(ResolvedEvent resolvedEvent)
+        {
+            if (_numberOfEvents != 0 && _numberOfEvents % 1000 == 0)
+                Log.Information("Number of events processed {NumberOfEvents}!", _numberOfEvents);
+            
+            _numberOfEvents++;
+            
+            return Task.CompletedTask;
+        }
+    }
+}
